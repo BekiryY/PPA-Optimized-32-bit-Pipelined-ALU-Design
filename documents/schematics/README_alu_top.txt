11d833edb38778602dd484222a71c48bef4e82d9
BekirYufka <bekiryufka@bekiryufka.net>
2025-12-16 23:15:16 +0300

    new toplevel schematic of module

---
+++ documents/schematics/README_alu_top.txt
@@ -0,0 +1,12 @@
+COLOR OF NETS AND INSTANCES:
+
+------- COLOR --------- MEANING -------
+---------------------------------------
+	CYAN		clk
+	ORANGE		reset_n
+	RED		CMD (opcode)
+	YELLOW		low_power-only modules
+	YELLOW		low_power mode selection signalde selection signal
+	BLUE		A & B inputs (operands)
+	BLUE		High performance modules
+	PURPLE 		output signals
--- rtl/SHIFTER.sv
+++ rtl/SHIFTER.sv
@@ -20,13 +20,7 @@ localparam CMD_BYT = 3'b110; // Byte Swap
 
 logic [32:0] temp_res;
 
-always_comb begin
-    // --- POWER GATING CLAMP/ISOLATION LOGIC ---
-    if (!power_en) begin
-        Y = 32'h0;      // Clamp outpuwt to 0 when sleeping (essential Isolation Cell function)
-        CF_flag = 1'b0;
-    end 
-    else begin
+i
         case (CMD)
             CMD_SLL: begin
                 temp_res = {1'b0, A} << B;
